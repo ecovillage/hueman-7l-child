ad91f5aa9501bbdfb2ce393ede79a417cfd93553
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-12-01 21:34:27 +0100

    extract event_row template part

---
+++ parts/event_row.php
@@ -0,0 +1,28 @@
+<tr>
+  <td class="datecol">
+    <?php echo date_i18n('d.m.Y', get_post_meta($post->ID, 'fromdate', true)); ?>
+    - <?php echo date_i18n('d.m.Y', get_post_meta($post->ID, 'todate', true)); ?>
+  </td>
+  <td class="eventcol">
+    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      <?php /* the_excerpt(); */ ?><br/>
+      <?php
+        $referees = referees_by_event($post->ID);
+        if ($referees->have_posts()) {
+          echo "(mit ";
+          while ($referees->have_posts()) {
+            $referees->the_post();
+            ?>
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <?php
+              // In all its madness, this is proposed by official documentation:
+              // https://codex.wordpress.org/Function_Reference/have_posts#Note
+              if ($referees->current_post + 1 < $referees->post_count) { echo ' und '; }
+          }
+          echo ")";
+        }
+        $events->reset_postdata();
+      ?>
+    </a>
+  </td>
+</tr>
--- single-ev7l-event-category.php
+++ single-ev7l-event-category.php
@@ -51,36 +51,8 @@
                     $monthyear = date_i18n('F Y', get_post_meta($post->ID, 'fromdate', true));
                     echo '</table><h2>'.$monthyear.'</h2><table>';
                   }
-                    ?>
-                    <tr>
-                      <td class="datecol">
-                        <?php echo date_i18n('d.m.Y', get_post_meta($post->ID, 'fromdate', true)); ?>
-                        - <?php echo date_i18n('d.m.Y', get_post_meta($post->ID, 'todate', true)); ?>
-                      </td>
-                      <td class="eventcol">
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                          <?php /* the_excerpt(); */ ?><br/>
-                          <?php
-                            $referees = referees_by_event($post->ID);
-                            if ($referees->have_posts()) {
-                              echo "(mit ";
-                              while ($referees->have_posts()) {
-                                $referees->the_post();
-                                ?>
-                                  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                <?php
-                                  // In all its madness, this is proposed by official documentation:
-                                  // https://codex.wordpress.org/Function_Reference/have_posts#Note
-                                  if ($referees->current_post + 1 < $referees->post_count) { echo ' und '; }
-                              }
-                              echo ")";
-                            }
-                            $events->reset_postdata();
-                          ?>
-                        </a>
-                      </td>
-                    </tr>
-              <?php }
+                  get_template_part('parts/event_row');
+               }
                 echo "</table>";
                 echo "</div><!-- .upcoming-events -->";
               }
