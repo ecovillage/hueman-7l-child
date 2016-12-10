a20fd917453ff29628a469e59140f28a9a17815e
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-12-10 21:52:20 +0100

    remove author/datebox below heading for own CPTs

---
+++ parts/single-heading.php
@@ -0,0 +1,5 @@
+<h1 class="post-title entry-title"><?php the_title(); ?></h1>
+<?php if (!(is_singular ( array ('ev7l-event', 'ev7l-event-category', 'ev7l-referee') ) ) ) {
+  get_template_part('parts/single-author-date');
+}
+?>
