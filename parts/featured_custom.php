c1f08821812b2e0d848d67e0c6083c8ca4f56e76
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-03-18 14:47:49 +0100

    let featured shortcode triggered slider only show 'news' posts.

--- parts/featured_custom.php
+++ parts/featured_custom.php
@@ -5,7 +5,8 @@ $featured = new WP_Query(
     		'no_found_rows'				   => false,
     		'update_post_meta_cache' => false,
     		'update_post_term_cache' => false,
-    		'ignore_sticky_posts'		 => 1,
+        'ignore_sticky_posts'		 => 1,
+        'category_name'          => 'news',
     		'posts_per_page'			   => 7
   	)
 );
