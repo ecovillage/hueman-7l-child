ff0f0b5064b415954a2ea8a32415c9b32d1c309c
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-05-23 11:12:38 +0200

    minor cleanups to still dirty registration page

--- parts/registration.php
+++ parts/registration.php
@@ -3,6 +3,12 @@
 $response = __("");
 $success  = false;
 
+$msg_missing_info   = __("Please provide all information");
+$msg_email_invalid  = __("Please provide a valid email adress");
+$msg_message_sent   = __("Message was sent..... if ... ");
+$msg_message_sent   = __("Message was not sent..... if ... ");
+$msg_need_tos       = __("You need to accept the tos and cancellation conditions");
+
 function registration_form_success($message) {
   global $response;
   $response = '<div class="success">'.$message.'</div>';
@@ -15,9 +21,23 @@ function registration_form_error($message) {
   $response = '<div class="error">'.$message.'</div>';
 };
 
-$missing_info  = __("Please provide all information");
-$email_invalid = __("Please provide a valid email adress");
-$message_sent  = __("Message was sent..... if ... ");
+function send_mail_to_host($registration) {
+  $to = 'bildungsreferat@siebenlinden.de';
+  $subject = 'Registration';
+  $headers = 'From: '. $registration['email'] . "\r\n" .
+    'Reply-To: ' . $registration['email'] . "\r\n";
+  $sent = wp_mail($to, $subject, strip_tags($registration['comments']), $headers);
+  if($sent) {
+    registration_form_success($msg_message_sent); //message sent!
+  }
+  else {
+    registration_form_error($msg_message_unsent); //message wasn't sent
+  }
+  return $sent;
+}
+
+function send_mail_to_participant($registration) {
+}
 
 // User provided content
 $firstname     = $_POST['firstname'];
@@ -30,6 +50,7 @@ $email         = $_POST['email'];
 $phone         = $_POST['phone'];
 $mobile        = $_POST['mobile'];
 $comment       = $_POST['comment'];
+$accept_tos    = $_POST['accept_tos'];
 
 #$registration[] = array(
 $registration = array(
@@ -50,9 +71,20 @@ $registration = array(
 # Missing fields: rooms, tos, donation, further participants
 
 if (false) {
+  if (!$accept_tos) {
+    registration_form_error($msg_need_tos);
+  }
+
   // validate (mail - adress, name, tos
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    registration_form_error($msg_email_invalid);
+  }
+  else {
+    # send mail
+  }
 }
 
+// Pseudo spam protection
 if ($_POST['submitted']) {
   file_put_contents("registration.txt", json_encode($registration));
   // mail to participant, mail to host
@@ -68,11 +100,14 @@ registration_form_error("Debug: ".var_export($registration));
 
     <hr/>
     <h2>__("Anmeldung")</h2>
+    <br/>
+    <br/>
 
     <span style="color:red;">
       <?php echo __("Bei technischen Problemen benuzte bitte unser "); ?>
       <a href="http://seminare.siebenlinden.de/seminar/1ee1ff4c-6a9d-11e6-9c78-78e7d1f4cba4"> Anmeldungen über alte Webseite hier möglich.</a>
     </span>
+    <br/>
     <form id="registration_form" action="<?php the_permalink(); ?>" charset="UTF-8" method="POST">
       <input type="hidden" name="seminar_id" value="1ee1ff4c-6a9d-11e6-9c78-78e7d1f4cba4"/>
 
@@ -118,57 +153,58 @@ registration_form_error("Debug: ".var_export($registration));
           <label for="comment"><?php echo __('Bemerkung'); ?></label>
         <textarea placeholder="..." id="comment" name="comment" rows="7"></textarea>
       </div>
+
+      <br class="clear"/>
+      <br/>
+
+      <h5><?php echo __('Übernachtung: Raumwünsche'); ?></h5>
+      <div class="grid one-half last">
+        <input type="checkbox" name="room_wish[]" id="4_Bett_Zimmer" value="4-Bett-Zimmer"/>
+        <label for="4_Bett_Zimmer">4-Bett-Zimmer</label>
+        <br/>
+
+        <input id='2_Bett_Zimmer' name='room_wish[]' type='checkbox' value='2-Bett-Zimmer'/>
+        <label for='2_Bett_Zimmer'>2-Bett-Zimmer</label>
+        <br/>
+
+        <input id='Einzelzimmer' name='room_wish[]' type='checkbox' value='Einzelzimmer'/>
+        <label for='Einzelzimmer'>Einzelzimmer</label>
+        <br/>
+
+        <input id='H_tte' name='room_wish[]' type='checkbox' value='Hütte'/>
+        <label for='H_tte'>Hütte</label>
+      </div>
+
+      <div class="grid one-half last">
+        <input id='Eigenes_Zelt' name='room_wish[]' type='checkbox' value='Eigenes Zelt'/>
+        <label for='Eigenes_Zelt'>Eigenes Zelt</label>
+        <br/>
+
+        <input id='Eigenes_Wohnmobil_wagen' name='room_wish[]' type='checkbox' value='Eigenes Wohnmobil/-wagen'/>
+        <label for='Eigenes_Wohnmobil_wagen'>Eigenes Wohnmobil/-wagen</label>
+        <br/>
+
+        <input id='Privat_Selbstorganisiert' name='room_wish[]' type='checkbox' value='Privat / Selbstorganisiert'/>
+        <label for='Privat_Selbstorganisiert'>Privat / Selbstorganisiert</label>
+      </div>
       <br class="clear"/>
 
-                    <br/>
-                    <h5><?php echo __('Übernachtung: Raumwünsche'); ?></h5>
-                    <div class="grid one-half last">
-                      <input type="checkbox" name="room_wish[]" id="4_Bett_Zimmer" value="4-Bett-Zimmer"/>
-                      <label for="4_Bett_Zimmer">4-Bett-Zimmer</label>
-                      <br/>
 
-                      <input id='2_Bett_Zimmer' name='room_wish[]' type='checkbox' value='2-Bett-Zimmer'/>
-                      <label for='2_Bett_Zimmer'>2-Bett-Zimmer</label>
-                      <br/>
+      <br/>
+      <br class="clear"/>
+      <br/>
+      <div class="registration-controls">
+      <h4><?php echo __('Rücktrittsbedingungen'); ?></h4>
+      <span class="cancel_conditions">Bei Rücktritt bis 28 Tage vor Seminarbeginn: keine Rücktrittsgebühr. Bei Rücktritt 28-14 Tage vor Seminarbeginn: 50 Eur Rücktrittsgebühr pro Person. Bei Rücktritt ab dem 14. Tag vor Seminarbeginn ist der volle Teilnahmebeitrag inkl. Unterkunftskosten zu zahlen. Bei Rücktritt ab 7 Tage vor Seminarbeginn oder Nichtteilnahme ohne Abmeldung ist der volle Teilnahmebeitrag inkl. Unterkunfts- und Verpflegungskosten zu zahlen.</span><br/>
+      <br/>
+      <input type="checkbox" id="accept_tos" name="accept_tos"><?php echo __('Ich akzeptiere die Rücktrittsbedingungen und die '); ?><a href="/seminare/agb/"><?php echo __('Allgemeinen Geschäftsbedingungen'); ?></a></input>
+      <br/>
+      <br/>
 
-                      <input id='Einzelzimmer' name='room_wish[]' type='checkbox' value='Einzelzimmer'/>
-                      <label for='Einzelzimmer'>Einzelzimmer</label>
-                      <br/>
-
-                      <input id='H_tte' name='room_wish[]' type='checkbox' value='Hütte'/>
-                      <label for='H_tte'>Hütte</label>
-                    </div>
-
-                    <div class="grid one-half last">
-                      <input id='Eigenes_Zelt' name='room_wish[]' type='checkbox' value='Eigenes Zelt'/>
-                      <label for='Eigenes_Zelt'>Eigenes Zelt</label>
-                      <br/>
-
-                      <input id='Eigenes_Wohnmobil_wagen' name='room_wish[]' type='checkbox' value='Eigenes Wohnmobil/-wagen'/>
-                      <label for='Eigenes_Wohnmobil_wagen'>Eigenes Wohnmobil/-wagen</label>
-                      <br/>
-
-                      <input id='Privat_Selbstorganisiert' name='room_wish[]' type='checkbox' value='Privat / Selbstorganisiert'/>
-                      <label for='Privat_Selbstorganisiert'>Privat / Selbstorganisiert</label>
-                    </div>
-                    <br class="clear"/>
-
-
-                    <br/>
-                    <br class="clear"/>
-                    <br/>
-                    <div class="registration-controls">
-                    <h4><?php echo __('Rücktrittsbedingungen'); ?></h4>
-                    <span class="cancel_conditions">Bei Rücktritt bis 28 Tage vor Seminarbeginn: keine Rücktrittsgebühr. Bei Rücktritt 28-14 Tage vor Seminarbeginn: 50 Eur Rücktrittsgebühr pro Person. Bei Rücktritt ab dem 14. Tag vor Seminarbeginn ist der volle Teilnahmebeitrag inkl. Unterkunftskosten zu zahlen. Bei Rücktritt ab 7 Tage vor Seminarbeginn oder Nichtteilnahme ohne Abmeldung ist der volle Teilnahmebeitrag inkl. Unterkunfts- und Verpflegungskosten zu zahlen.</span><br/>
-                    <br/>
-                    <input type="checkbox" id="accept_terms" name="registration[accept_terms]">Ich akzeptiere die Rücktrittsbedingungen und die <a href="/seminare/agb/">Allgemeinen Geschäftsbedingungen</a></input>
-                    <br/>
-                    <br/>
-
-                    <input type="hidden" name="submitted" value="1">
-                    <input type="submit" value="<?php echo __('Anmelden'); ?>"></submit>
-                    </div>
-                  </form>
+      <input type="hidden" name="submitted" value="1">
+      <input type="submit" value="<?php echo __('Anmelden'); ?>"></submit>
+      </div>
+    </form>
    <?php } /* !$success */ ?>
 
   </div> <!-- #registration -->
