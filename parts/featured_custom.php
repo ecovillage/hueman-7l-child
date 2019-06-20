716ef0ebb11aa732842f4043cc48eee2d526ed28
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-06-20 12:25:11 +0200

    use php, not js syntax for localized news category

--- parts/featured_custom.php
+++ parts/featured_custom.php
@@ -1,8 +1,8 @@
 <?php
 // Query featured entries
-var localized_category_name = 'news';
+$localized_category_name = 'news';
 if ($lang == 'en') {
-  localized_category_name = 'news-en';
+  $localized_category_name = 'news-en';
 }
 
 $featured = new WP_Query(
@@ -11,7 +11,7 @@ $featured = new WP_Query(
     		'update_post_meta_cache' => false,
     		'update_post_term_cache' => false,
         'ignore_sticky_posts'		 => 1,
-        'category_name'          => localized_category_name,
+        'category_name'          => $localized_category_name,
     		'posts_per_page'			   => 7
   	)
 );
