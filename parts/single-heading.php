fd65b56d73fdcf2ecbca9662812c7ff25a8c8f29
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-09-23 16:34:21 +0200

    towards seminardesk-wordpress compatibility (#7)

--- parts/single-heading.php
+++ parts/single-heading.php
@@ -1,5 +1,6 @@
 <h1 class="post-title entry-title"><?php the_title(); ?></h1>
-<?php if (!(is_singular ( array ('ev7l-event', 'ev7l-event-category', 'ev7l-referee') ) ) ) {
+<?php if (!(is_singular ( array ('ev7l-event', 'ev7l-event-category',
+  'ev7l-referee', 'sd_cpt_event', 'sd_cpt_date', 'sd_cpt_facilitator' ) ) ) ) {
   get_template_part('parts/single-author-date');
 }
 ?>
