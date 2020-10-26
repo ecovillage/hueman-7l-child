641450e1d41ffd033f65f9795c0fde5c56fc5b22
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-10-26 10:35:41 +0100

    update translations

--- seminardesk-custom/templates/sd_txn_dates.php
+++ seminardesk-custom/templates/sd_txn_dates.php
@@ -67,15 +67,15 @@ get_header();
                     <div class="sd-event-container">
                         <div class="sd-event-props">
                             <?php
-                            Utils::get_date( $post->sd_date_begin, $post->sd_date_end, '<div class="sd-event-date">' . __('<strong>Date: </strong>', 'seminardesk'), '</div>', true);
-                            Utils::get_facilitators($post_event->sd_data['facilitators'], '<div class="sd-event-facilitators"><strong>' . __('Facilitator: ', 'seminardesk') . '</strong>', '</div>', true);
-                            Utils::get_value_by_language($post->sd_data['priceInfo'], 'DE', '<div class="sd-event-price"><strong>' . __('Price: ', 'seminardesk') . '</strong>', '</div>', true );
-                            Utils::get_venue($post->sd_data['venue'], '<div class="sd-event-venue"><strong>' . __('Venue: ', 'seminardesk') . '</strong>', '</div>', true);
+                            Utils::get_date( $post->sd_date_begin, $post->sd_date_end, '<div class="sd-event-date">' . __('<strong>Date: </strong>', 'hueman-7l-child'), '</div>', true);
+                            Utils::get_facilitators($post_event->sd_data['facilitators'], '<div class="sd-event-facilitators"><strong>' . __('Facilitator: ', 'hueman-7l-child') . '</strong>', '</div>', true);
+                            Utils::get_value_by_language($post->sd_data['priceInfo'], 'DE', '<div class="sd-event-price"><strong>' . __('Price: ', 'hueman-7l-child') . '</strong>', '</div>', true );
+                            Utils::get_venue($post->sd_data['venue'], '<div class="sd-event-venue"><strong>' . __('Venue: ', 'hueman-7l-child') . '</strong>', '</div>', true);
                             ?>
                         </div>
                         <div class=sd-event-image>
                             <?php
-                            Utils::get_img_remote(  Utils::get_value_by_language($post_event->sd_data['teaserPictureUrl']), '300', '', $alt = __('remote image failed', 'seminardesk'), '', '', true);
+                            Utils::get_img_remote(  Utils::get_value_by_language($post_event->sd_data['teaserPictureUrl']), '300', '', $alt = __('remote image failed', 'hueman-7l-child'), '', '', true);
                             ?>
                         </div>
                         <div class=sd-event-teaser>
@@ -84,7 +84,7 @@ get_header();
                             ?>
                             <div class="sd-event-more-link">
                                 <a class="button" href="<?php echo esc_url(get_permalink($post->wp_event_id)); ?>">
-                                    <?php esc_html_e('More', 'seminardesk')?>
+                                    <?php esc_html_e('More', 'hueman-7l-child')?>
                                 </a>
                             </div> 
                         </div>
@@ -103,7 +103,7 @@ get_header();
         <div class="entry-header-inner section-inner small has-text-align-center">
             <h5>
                 <?php
-                _e('Sorry, no events for this date.', 'seminardesk');
+                _e('Sorry, no events for this date.', 'hueman-7l-child');
                 ?>
             </h5>
             <br>
