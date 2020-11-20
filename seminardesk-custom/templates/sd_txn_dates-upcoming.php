a3ce79ccec58e71a0226a7dd9e89127e6cfeec06
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-11-20 16:15:48 +0100

    try to make upcoming txn_dates look like other txn_dates

--- seminardesk-custom/templates/sd_txn_dates-upcoming.php
+++ seminardesk-custom/templates/sd_txn_dates-upcoming.php
@@ -40,38 +40,60 @@ get_header();
             $post_event = get_post( $post->wp_event_id );
             $post_event_status = $post_event->post_status;
             ?>
-            <div class="entry-header-inner section-inner small">
+            <div class="sd-event">
+              <div class="entry-header-inner section-inner small">
                 <?php
                 $term = get_the_terms( $post, 'sd_txn_dates' );
                 if ($term['0']->description != $term_set ){
                     $term_set = $term['0']->description;
                     echo '<h4>' . $term_set . '</h4>';
                 }
-                if ( $post_event_status === 'publish' ){
-                    ?>
-                    <a href="<?php echo get_permalink($post_event); ?>">
-                    <?php 
-                    Utils::get_value_by_language( $post_event->sd_data['title'], 'DE', '<h4>', '</h4>', true); 
-                    ?>
-                </a>
-                <?php
-                } else {
-                    Utils::get_value_by_language( $post_event->sd_data['title'], 'DE', '<h4>', '</h4>', true);
-                }
-                Utils::get_date( $post->sd_data['beginDate'], $post->sd_data['endDate'], '<p><strong>Date: </strong>', '</p>', true);
-                Utils::get_facilitators( $post_event->sd_data['facilitators'], '<p><strong>Facilitator: </strong>', '</p>', true );
-                echo Utils::get_value_by_language( $post->sd_data['priceInfo'], 'DE', '<p><strong>Price: </strong>', '</p>' );
-                Utils::get_venue( $post->sd_data['venue'], '<p><strong>Venue: </strong>', '</p>', true);
-                Utils::get_img_remote( Utils::get_value_by_language( $post_event->sd_data['teaserPictureUrl'] ?? null ), '300', '', $alt = "remote image load failed", '<p>', '</p>', true );
-                Utils::get_value_by_language( $post_event->sd_data['teaser'], 'DE',  '<p>', '</p>', true );
-                if ( $post_event_status === 'publish' ){
-                    ?>
-                    <a href="<?php echo get_permalink($post->wp_event_id); ?>">
-                      <?php _e( 'More', 'hueman-7l-child' ); ?>
-                    </a>
-                    <?php
-                } 
                 ?>
+                <div class="sd-event-title"><?php
+                  if ( $post_event_status === 'publish' ){
+                      ?>
+                      <a href="<?php echo get_permalink($post_event); ?>">
+                      <?php 
+                      Utils::get_value_by_language( $post_event->sd_data['title'], 'DE', '<h4>', '</h4>', true); 
+                      ?>
+                  </a>
+                  <?php
+                  } else {
+                      Utils::get_value_by_language( $post_event->sd_data['title'], 'DE', '<h4>', '</h4>', true);
+                  }
+                ?>
+                </div>
+
+                <div class="sd-event-container">
+                  <div class="sd-event-props">
+                    <?php
+                      Utils::get_date( $post->sd_data['beginDate'], $post->sd_data['endDate'], '<p><strong>Date: </strong>', '</p>', true);
+                      Utils::get_facilitators( $post_event->sd_data['facilitators'], '<p><strong>Facilitator: </strong>', '</p>', true );
+                      echo Utils::get_value_by_language( $post->sd_data['priceInfo'], 'DE', '<p><strong>Price: </strong>', '</p>' );
+                      Utils::get_venue( $post->sd_data['venue'], '<p><strong>Venue: </strong>', '</p>', true);
+                    ?>
+                  </div> <!-- sd-event-props -->
+
+                  <div class="sd-event-image">
+                    <?php
+                      Utils::get_img_remote( Utils::get_value_by_language( $post_event->sd_data['teaserPictureUrl'] ?? null ), '300', '', $alt = "remote image load failed", '<p>', '</p>', true );
+                    ?>
+                  </div> <!-- sd-event-image -->
+
+                  <div class="sd-event-teaser">
+                    <?php
+                      Utils::get_value_by_language( $post_event->sd_data['teaser'], 'DE',  '<p>', '</p>', true );
+                      if ( $post_event_status === 'publish' ){
+                          ?>
+                          <a href="<?php echo get_permalink($post->wp_event_id); ?>">
+                            <?php _e( 'More', 'hueman-7l-child' ); ?>
+                          </a>
+                          <?php
+                      }
+                    ?>
+                  </div> <!-- sd-event-teaser -->
+                </div> <!-- sd-event-container -->
+              </div>
             </div>
             <?php 
         }?>
