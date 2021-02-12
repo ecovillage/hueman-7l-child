51bdd3fb6bcb4966295d09be15a4df99bf51b915
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2021-02-12 16:15:07 +0100

    change js/css links as they dont work on all FS

--- parts/sd_cpt_event.js.php
+++ parts/sd_cpt_event.js.php
@@ -1,1 +1,29 @@
-../seminardesk-custom/templates/assets/sd_cpt_event.js
+/**
+ * JavaScript for single template of CPT sd_cpt_event
+ * 
+ * @package SeminardeskPlugin
+ */
+
+// get the modal and its content
+var modal = document.querySelector(".sd-modal");
+var btnBooking = document.querySelector(".sd-modal-booking-btn");
+// get button that closes the modal
+var btnClose = document.querySelector(".sd-modal-close-btn");
+
+// toggle between show and hide of the modal
+function sdModalToggle() {
+    modal.classList.toggle("sd-modal-show");
+}
+function windowOnClick(event) {
+    if (event.target === modal) {
+        sdModalToggle();
+    }
+}
+
+// click events listener
+if ( btnBooking !== null && modal !== null ){
+    btnBooking.addEventListener("click", sdModalToggle);
+    btnClose.addEventListener("click", sdModalToggle);
+    window.addEventListener("click", windowOnClick);
+}
+
