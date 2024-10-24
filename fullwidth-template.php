9e8e650a7fdac2401103ba88186d2be0306e1176
jens <48951041+js32@users.noreply.github.com>
2024-10-24 08:28:01 +0000

    Create fullwidth-template.php
    + template for full-width pages

---
+++ fullwidth-template.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Template Name: fullwidth-template
+* you guessed it
+*/
+?><!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+
+<?php get_header('fullwidth-template'); ?>
+
+<?php hu_get_content( 'tmpl/page-tmpl'); ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
+</html>
